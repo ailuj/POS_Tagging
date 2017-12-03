1f734916b3d7d7409658976d2f0a6d028353be23
Julia Dullin <juliadullin@Julias-iMac.local>
2017-12-03 13:36:22 +0100

    bugfixes

--- Tagging.java
+++ Tagging.java
@@ -13,11 +13,18 @@ public class Tagging {
         if(args[0].equals("annotate")) {
             File folder = new File(args[1]);
             File outputDir = new File(args[2]);
-            List<String> wordList = splitText(folder);
-            String[] words = wordList.toArray(new String[wordList.size()]);
+            List<String> filenames = new ArrayList<String>();
+            for (File file : folder.listFiles()) {
+                filenames.add(file.getName());
+            }
+            String[][] words = splitText(folder);
             Tagging v = new Tagging();
-            String tags = v.viterbi(words);
-            writeLineToFile(outputDir, tags);
+            for (int x = 0; x < words.length; x++){
+                String tags = v.viterbi(words[x]);
+                File outputFile = new File(outputDir + "/" + filenames.get(x));
+                writeLineToFile(outputFile, tags);
+            }
+
         }
     }
 
@@ -43,11 +50,13 @@ public class Tagging {
         }
     }
 
-    private static List<String> splitText(File folder){
+    private static String[][] splitText(File folder){
         BufferedReader br = null;
         FileReader fr = null;
-        List<String> temp = new ArrayList<String>();
+        String[][] files = new String[folder.listFiles().length][];
+        int count = 0;
         for (File file : folder.listFiles()) {
+            List<String> temp = new ArrayList<String>();
             try {
                 br = new BufferedReader(new FileReader(file));
                 String s = null;
@@ -70,8 +79,10 @@ public class Tagging {
             String[] parts = null;
             e.printStackTrace();
         }
+        files[count] = temp.toArray(new String[temp.size()]);
+        count += 1;
         }
-        return temp;
+        return files;
     }
 
     private static int[] deepCopyIntArray(int[] oldArray){
@@ -93,16 +104,10 @@ public class Tagging {
 
     public static String viterbi(String[] words){
         Node[] n = new Node[tags.length];
-        double emit;
         for (int state = 0; state < tags.length; state++){
             int[] intArray = new int[1];
             intArray[0] = state;
-            if (model.containsEmiss(tags[state], words[0]) == 1){
-                emit = model.getEmiss(tags[state], words[0]);
-            }else{
-                emit = 0.0;
-            }
-            n[state] = new Node(intArray, model.getStart(tags[state]) * emit);
+            n[state] = new Node(intArray, model.getStart(tags[state]) * model.getEmiss(tags[state], words[0]));
         }
 
         for (int output = 1; output < words.length; output++){
@@ -113,13 +118,7 @@ public class Tagging {
                 for (int state = 0; state < tags.length; state++){
                     int[] path = deepCopyIntArray(n[state].path);
                     double prob = n[state].prob;
-                    if(model.containsEmiss(tags[nextState], words[output]) == 1){
-                        emit = model.getEmiss(tags[nextState], words[output]);
-                    } else {
-                        emit = 0.0;
-                    }
-                    double p = emit * model.getTrans(tags[state], tags[nextState]);
-
+                    double p = model.getEmiss(tags[nextState], words[output]) * model.getTrans(tags[state], tags[nextState]);
                     prob *= p;
                     if(prob > valMax){
                         if(path.length == words.length){
@@ -147,7 +146,7 @@ public class Tagging {
         String viterbiPath = "";
         for(int i = 0; i < argMax.length; i++){
             String tagged = words[i] + "/" + tags[argMax[i]] + " ";
-            System.out.print(words[i] + "/" + tags[argMax[i]] + " ");
+            //System.out.print(words[i] + "/" + tags[argMax[i]] + " ");
             viterbiPath += tagged;
         }
         return viterbiPath;
